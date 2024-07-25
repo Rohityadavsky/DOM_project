7f9cb57da7d7041e480f8660fa8d8892e94bdaa4
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-07-25 23:02:07 +0530

    adding project

---
+++ script.js
@@ -0,0 +1,17 @@
+let projectId = document.getElementById('projectId');
+
+projectId.style.backgroundColor = 'cyan';
+
+projectId.style.margin = '40px';
+
+projectId.style.padding = '20px';
+
+projectId.style.fontSize = '20px';
+
+projectId.style.fontWeight = 'bold';
+
+projectId.style.height = '200px';
+
+projectId.style.width = '300px';
+
+console.log(projectId);
